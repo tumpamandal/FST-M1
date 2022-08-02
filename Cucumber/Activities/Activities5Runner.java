aae9ddf42a5b26dc2dfc05ba2fa5f08aaac13724
tumpamandal <106962023+tumpamandal@users.noreply.github.com>
2022-08-02 22:48:23 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activities5Runner.java
@@ -0,0 +1,13 @@
+package testRunner;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features",
+        glue = {"stepDefinitions"},
+        tags = "@activity5"
+)
+
+public class Activities5Runner {
+}
