35616bd40b8189f307ae1a5af27f71238284192d
tumpamandal <106962023+tumpamandal@users.noreply.github.com>
2022-08-03 10:32:43 +0530

    Add files via upload

---
+++ Appium/Activities/AndroidDriver.java
@@ -0,0 +1,9 @@
+package Appium_Activities;
+
+public class AndroidDriver {
+    public void get(String s) {
+    }
+
+    public void quit() {
+    }
+}
---
+++ Appium/Activities/UiAutomator2Options.java
@@ -0,0 +1,18 @@
+package Appium_Activities;
+
+public class UiAutomator2Options {
+    public void setPlatformName(String android) {
+    }
+
+    public void setAutomationName(String uiAutomator2) {
+    }
+
+    public void setAppPackage(String s) {
+    }
+
+    public void setAppActivity(String s) {
+    }
+
+    public void noReset() {
+    }
+}
